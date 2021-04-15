8bbf75475e0099755eff95c7beddc55efd4565dc
AlexWithCode <adzhuzhukova@gmail.com>
2021-04-15 22:30:48 +0200

    first commit

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,12 +11,30 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 150;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'снегурочка';
+  
+ // To set a List Filter
+    //declare a private backing variable to hold the value   
+    private _listFilter: string = '';
+    //define the getter
+    get listFilter(): string {
+        //the body includes code to process the property value before returning it
+        return this._listFilter;
+    }
+    //define the setter. It has a single parameter, the value assigned to the property. The setter has no return value
+    set listFilter(value: string) {
+        //to perform an operation when the property is changed we set the value into the private backing variable
+        console.log('In sett', value);
+        //set a property to filtered list of products and defined a method to filter our products, which added down here by the other methods
+        this.filteredProducts = this.performFilter(value);
+    }
+
+    //define a property to get a filtered list of products that we can bind to
+    filteredProducts: IProduct[] = [];
     products: IProduct[] = [
         { 
             "productId": 2,
             "productName": "Снегурочка Снежинка",
-            "productCode": "SN-S-01",
+            "productCode": "SNSx-01",
             "sale": "10 %",
             "dateSale": "31.12.2021",
             "description": "Атласная белая ткань с металлическим отливом, круппные узоры, застежки на липучках. В комплекте шубка, варежки, шапочка.",
@@ -27,7 +45,7 @@ export class ProductListComponent implements OnInit {
         {
             "productId": 5,
             "productName": "Дед Мороз Боярский Синий",
-            "productCode": "DM-BS-01",
+            "productCode": "DMBS-01",
             "sale": "",
             "dateSale": "",
             "description": "Бархатная мягкая синяя ткань с длинношерстным мехом, блестящими крупными узорами и вшитыми бусинами. В комплекте шуба, варежки, шапка, пояс, мешок.",
@@ -38,11 +56,21 @@ export class ProductListComponent implements OnInit {
 
     ];
 
+    //method takes in the one parameter, which is string and returns the filtered array of products
+    performFilter(filterBy: string): IProduct[] {
+        //the code starts by converting the filter criteria to lowercase for a case-insencitive comparsion
+        filterBy = filterBy.toLocaleLowerCase();
+        //return the filtered list of products: we start with the original full list of products, an array, we use the JS arrow method "filter", we pass into the filter method an arrow function that processes each passed-in product. In the body of the arrow function we check the productName
+        return this.products.filter((product: IProduct) => 
+        //we call the string includes method, passing in the filter string, which returns true if the productName includes the defined filter string
+        product.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log("In OnInit");
+        this.listFilter = '';
     }
 }
